ed9e2b847378e4e675a3fccae131860fdca12375
Kamila Souckova <kamila@vesmir.sk>
2014-11-02 18:25:28 +0100

    site.hs: use .||. instead of forM_ on list

--- site.hs
+++ site.hs
@@ -7,7 +7,6 @@ config = defaultConfiguration { deployCommand = "rsync -avh _site/ ksp:~/www" }
 main = hakyllWith config $ do
     -- TODO something smart
 
-    -- just copy the rest
-    forM_ [ "assets/*", "stuff/*", "snippets/*" ]
-          (\p -> match p $ do route   idRoute
-                              compile copyFileCompiler)
+    match ("assets/*" .||. "stuff/*" .||. "snippets/*") $ do
+        route   idRoute
+        compile copyFileCompiler
